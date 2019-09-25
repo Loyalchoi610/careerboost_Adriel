89fc574a2b46c04b2f42b5f3b80941f5ebc62557
Choi Seungeon <chou610@naver.com>
2019-09-25 09:06:40 +0900

    initial commit

---
+++ seungeon_1st/src/store/modules/todo/index.js
@@ -0,0 +1,8 @@
+export const Todo = {
+  state: {},
+  mutations: {
+  },
+  actions: {
+
+  }
+}
---
+++ seungeon_1st/src/views/TodoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList'
+}
+</script>
+
+<style scoped>
+</style>
