25fb1ee133c318140fb95a3f5964e7927771cd30
Choi Seungeon <chou610@naver.com>
2019-09-29 08:59:48 +0900

    [Update] dotenv 파일에서 민감한 정보 로딩

--- seungeon_1st/src/store/index.js
+++ seungeon_1st/src/store/index.js
@@ -4,19 +4,19 @@ import { Todo } from './modules/todo'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 Vue.use(Vuex)
-let firebaseConfig = {
-  apiKey: 'AIzaSyA3r63OBSnMXnFsQ466pY5I6nxUtvX8Rtk',
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APP_KEY,
   authDomain: 'adriel-vue.firebaseapp.com',
   databaseURL: 'https://adriel-vue.firebaseio.com',
   projectId: 'adriel-vue',
   storageBucket: 'adriel-vue.appspot.com',
-  messagingSenderId: '112051604813',
-  appId: '1:112051604813:web:9ff24dfe0cacf0433b3b04'
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 firebase.initializeApp(firebaseConfig)
 export default new Vuex.Store({
   state: {
-    db: firebase.firestore()
+    DB: firebase.firestore()
   },
   mutations: {
 
